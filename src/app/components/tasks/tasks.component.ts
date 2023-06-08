d593208f37b23e70fa659a00d629194400d58c40
sayedhany <sayed591999@gmail.com>
2023-06-08 23:28:58 +0300

    finish

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from'../../mock-tasks';
+// import {TASKS} from'../../mock-tasks';
 import { Task } from 'src/app/Task';
-
-
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  
-  constructor(){
+  tasks: Task[] = [];
 
-}
+  constructor(private taskService: TaskService) {}
   ngOnInit(): void {
-
-}
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+  onDeleteTaks(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter((t) => t.id !== task.id);
+    });
+  }
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => {
+      this.tasks.push(task);
+    });
+  }
 }
--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,9 +1,15 @@
-import { Task } from "./Task"
+import { Task } from './Task';
 export const TASKS: Task[] = [
   {
     id: 1,
-    text: "Finish ang course",
-    day: "May 5th at 2:30pm",
-    reminder: true
-  }
-]
+    text: 'Finish ang course',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Finish ang project',
+    day: 'May 25th at 8:30pm',
+    reminder: true,
+  },
+];
